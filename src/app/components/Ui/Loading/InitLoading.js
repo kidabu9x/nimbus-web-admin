798f6f62584a477f3df5053b1418386001d38c37
kidabu9x <duongnk.techkids@gmail.com>
2020-06-04 00:01:42 +0700

    NS-72: add suspend

--- src/app/components/Ui/Loading/InitLoading.js
+++ src/app/components/Ui/Loading/InitLoading.js
@@ -1,24 +1,50 @@
 import React from "react";
 import { ReactComponent as Logo } from "../../../static/images/svg/logo.svg";
 import { makeStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import {
+    LinearProgress,
+    Grid
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        width: '100%',
-        '& > * + *': {
-            marginTop: theme.spacing(2),
-        },
+        minHeight: "60vh"
     },
+    container: {
+        maxWidth: "200px"
+    },
+    logo: {
+        width: "200px",
+        height: "auto",
+        marginBottom: theme.spacing(1)
+    },
+    colorPrimary: {
+        backgroundColor: '#207347',
+    },
+    barColorPrimary: {
+        backgroundColor: '#6FBF94',
+    }
 }));
 
 export default function LinearDeterminate() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <Logo />
-            <LinearProgress />
-        </div>
+        <Grid
+            className={classes.root}
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+        >
+            <Grid item xs={3}>
+                <div className={classes.container}>
+                    <Logo className={classes.logo} />
+                    <LinearProgress classes={{ colorPrimary: classes.colorPrimary, barColorPrimary: classes.barColorPrimary }} />
+                </div>
+            </Grid>
+        </Grid>
+
     );
 }
